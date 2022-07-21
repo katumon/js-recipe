5bde8d592e5a45743b893645e63d1ae2b932f45b
Reika <reika918@icloud.com>
2022-07-21 10:10:11 +0900

    メモアプリ完成

--- 04_ゼミ/week2/session/main.js
+++ 04_ゼミ/week2/session/main.js
@@ -1,10 +1,10 @@
 
-const memoContainer = document.getElementById("memo-container") /*inputをvalueプロパティで受け取る*/
+const memoContainer = document.getElementById("memo-container") 
 const addButton = document.getElementById("add-button")
- /*class名を引数にしている！*/
+
 
 addButton.onclick = function(){
-    const memoInput = document.getElementById("memo-input").value
+    const memoInput = document.getElementById("memo-input").value /*inputをvalueプロパティで受け取る*/
     let memo = document.createElement("div")
     memo.textContent = memoInput
     memoContainer.append(memo)
